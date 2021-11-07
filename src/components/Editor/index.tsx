391822bc9ab43fd2a0fd4851b8c83f6c28e878c0
AdrianTodt <adriantodt.ms@gmail.com>
2021-11-07 15:34:16 -0300

    Initial CodeMirror integration

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,4 +1,8 @@
 import styles from './index.module.css';
+import './codemirror.css';
+import { useEffect, useRef } from 'react';
+import { useCodeMirror } from '@uiw/react-codemirror';
+import { lineNumbers } from '@codemirror/gutter';
 
 export interface EditorProps {
   code: string;
@@ -7,5 +11,29 @@ export interface EditorProps {
 }
 
 export default function Editor(props: EditorProps) {
-  return <textarea className={styles.editor} value={props.code} onChange={event => props.setCode(event.target.value)}/>;
+  const editor = useRef<HTMLDivElement>(null);
+  const { setContainer } = useCodeMirror({
+    container: editor.current,
+    extensions: [],
+    value: props.code,
+    height: '100%',
+    width: '100%',
+    maxHeight: '100%',
+    maxWidth: '100%',
+    theme: 'dark',
+    basicSetup: true,
+    onChange(value: string) {
+      props.setCode(value);
+    },
+  });
+
+  useEffect(() => {
+    if (editor.current) {
+      setContainer(editor.current);
+    }
+  }, [setContainer]);
+
+  return <div ref={editor} className={styles.cme}/>;
+
+  // return <textarea className={styles.editor} value={props.code} onChange={event => props.setCode(event.target.value)}/>;
 }
